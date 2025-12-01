7f75de0f84b552d6f3ebcaa3994dd006c92e41dc
Kenura R. Gunarathna <kenuragunarathna@gmail.com>
2025-12-01 12:03:55 +0530

    updated tests composer

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,8 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
+    'Kenura\\Imagick\\Tests\\' => array($baseDir . '/tests'),
     'Kenura\\Imagick\\' => array($baseDir . '/src'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInitab01c140e9df8a38583576052eee0e61
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitab01c140e9df8a38583576052eee0e61::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
